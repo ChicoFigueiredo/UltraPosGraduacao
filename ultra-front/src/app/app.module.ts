98cc496bb4965e43279c95d0e997ab88972c9ecb
Francisco Lima Figueiredo <fran.fig@gmail.com>
2018-04-12 09:13:21 -0300

    Chico: adicionando formulário e corrigindo encoding cagado

--- ultra-front/src/app/app.module.ts
+++ ultra-front/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { FormInscricaoComponent } from './component/form-inscricao/form-inscricao.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormInscricaoComponent
   ],
   imports: [
     BrowserModule,

12f5e6996d266c822753b0018caa98660b1e1150
Francisco Lima Figueiredo (Chicão) <francisco.figueiredo@caixa.gov.br>
2018-04-16 19:55:24 -0300

    Chico: ajustar credit card

--- ultra-front/src/environments/environment.ts
+++ ultra-front/src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  rootApi : '', //'http://inscricao.ultraposgraduacao/'
+  rootApi : 'http://inscricao.ultraposgraduacao.com.br/', //'http://inscricao.ultraposgraduacao/'
 };

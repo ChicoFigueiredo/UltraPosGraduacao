6548ea3b86c9752a8261f095cc1a85347347d034
Francisco 'Chico' Lima Figueiredo <fran.fig@gmail.com>
2019-06-16 14:03:58 -0300

    Chico: rxJs NG UPDATE @Angular/Core

--- src/app/component/form-inscricao/form-inscricao.component.ts
+++ src/app/component/form-inscricao/form-inscricao.component.ts
@@ -32,15 +32,15 @@ export class FormInscricaoComponent implements OnInit, DoCheck {
 
   displayForm = 'initial';
 
-  @ViewChild('erroCPF') private erroCPF: ElementRef;
-  @ViewChild('selMatricula') private selMatricula: ElementRef;
-  @ViewChild('selMensalidade') private selMensalidade: ElementRef;
-  @ViewChild('txtCupom') private txtCupom: ElementRef;
-  @ViewChild('btnCupom') private btnCupom: ElementRef;
-  @ViewChild('formAluno') private formAluno;
-  @ViewChild('TituloModal') private TituloModal: ElementRef;
-  @ViewChild('msgRetorno') private msgRetorno: ElementRef;
-  @ViewChild('ifrmMensagem') private ifrmMensagem: ElementRef;
+  @ViewChild('erroCPF', { static: false }) private erroCPF: ElementRef;
+  @ViewChild('selMatricula', { static: true }) private selMatricula: ElementRef;
+  @ViewChild('selMensalidade', { static: true }) private selMensalidade: ElementRef;
+  @ViewChild('txtCupom', { static: true }) private txtCupom: ElementRef;
+  @ViewChild('btnCupom', { static: true }) private btnCupom: ElementRef;
+  @ViewChild('formAluno', { static: true }) private formAluno;
+  @ViewChild('TituloModal', { static: true }) private TituloModal: ElementRef;
+  @ViewChild('msgRetorno', { static: true }) private msgRetorno: ElementRef;
+  @ViewChild('ifrmMensagem', { static: false }) private ifrmMensagem: ElementRef;
 
   constructor(
     public alunoService: ApiUltraService,
